251edacbf1f0db766afa2a345881d13b773f8951
Sebastian Lech <lechum2@gmail.com>
2022-01-19 13:59:20 +0100

    Add formatting plugin and use styLua

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,33 +1,35 @@
-return require('packer').startup(function()
-		use 'wbthomason/packer.nvim'
-	
-		use {
-				'nvim-treesitter/nvim-treesitter',
-				run = ':TSUpdate'
-		}
+return require("packer").startup(function()
+	use("wbthomason/packer.nvim")
 
-		use 'tpope/vim-fugitive'
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = ":TSUpdate",
+	})
 
-		--telescope
-		use 'kyazdani42/nvim-web-devicons'
-		use 'nvim-lua/popup.nvim'
-		use 'nvim-lua/plenary.nvim'
-		use 'nvim-telescope/telescope.nvim'
-		use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+	use("tpope/vim-fugitive")
 
-		--LSP
-		use 'neovim/nvim-lspconfig'
-		use 'hrsh7th/cmp-nvim-lsp'
-		use 'hrsh7th/cmp-buffer'
-		use 'hrsh7th/cmp-path'
-		use 'hrsh7th/cmp-cmdline'
-		use 'hrsh7th/nvim-cmp'
-		use 'hrsh7th/cmp-vsnip'
-		use 'hrsh7th/vim-vsnip'
+	--telescope
+	use("kyazdani42/nvim-web-devicons")
+	use("nvim-lua/popup.nvim")
+	use("nvim-lua/plenary.nvim")
+	use("nvim-telescope/telescope.nvim")
+	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
 
-		use 'mbbill/undotree'
+	--LSP
+	use("neovim/nvim-lspconfig")
+	use("hrsh7th/cmp-nvim-lsp")
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/cmp-path")
+	use("hrsh7th/cmp-cmdline")
+	use("hrsh7th/nvim-cmp")
+	use("hrsh7th/cmp-vsnip")
+	use("hrsh7th/vim-vsnip")
 
-		--Themes
-		use 'NLKNguyen/papercolor-theme'
-		use 'Shatur/neovim-ayu'
+	use("sbdchd/neoformat")
+
+	use("mbbill/undotree")
+
+	--Themes
+	use("NLKNguyen/papercolor-theme")
+	use("Shatur/neovim-ayu")
 end)
