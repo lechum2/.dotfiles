925dcb903b578a0d5157afc6194c2c094d664e09
Sebastian Lech <lechum2@gmail.com>
2022-07-05 14:23:35 +0200

    Don't show spelling completion - it's anoying

--- .config/nvim/lua/myNullLsConfig.lua
+++ .config/nvim/lua/myNullLsConfig.lua
@@ -5,6 +5,5 @@ require("null-ls").setup({
 		require("null-ls").builtins.formatting.prettier,
 		require("null-ls").builtins.diagnostics.eslint,
 		require("null-ls").builtins.diagnostics.cspell,
-		require("null-ls").builtins.completion.spell,
 	},
 })
