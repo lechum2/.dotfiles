c084781147fd50a4199ac76e5cca6086d5df866b
Sebastian Lech <lechum2@gmail.com>
2022-07-24 07:34:07 +0200

    Change tab characters

--- .config/nvim/plugin/indentBlankline.lua
+++ .config/nvim/plugin/indentBlankline.lua
@@ -1,7 +1,7 @@
 vim.opt.list = true
 vim.opt.listchars:append("space:⋅")
 vim.opt.listchars:append("eol:↴")
-vim.opt.listchars:append("tab:>-")
+vim.opt.listchars:append("tab:| ⇥")
 
 require("indent_blankline").setup({
 	show_end_of_line = true,
