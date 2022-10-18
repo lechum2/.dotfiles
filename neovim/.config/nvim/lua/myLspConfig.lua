cd5a266aa66f503d518f2a7d966f11e1ff38c2f4
Sebastian Lech <lechum2@gmail.com>
2022-10-18 10:34:44 +0200

    update configuration after package changes

--- neovim/.config/nvim/lua/myLspConfig.lua
+++ neovim/.config/nvim/lua/myLspConfig.lua
@@ -50,7 +50,7 @@ cmp.setup.cmdline(":", {
 
 -- Setup lspconfig.
 local lspconfig = require("lspconfig")
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 require("apexls")
 local servers = { "tsserver", "sumneko_lua", "apexls" }
 for _, lsp in ipairs(servers) do
