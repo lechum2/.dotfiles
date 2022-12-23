f062708338f6646c04baf78b8007142b1643365b
Sebastian Lech <lechum2@gmail.com>
2022-12-23 15:00:37 +0100

    Add function to search over dotfiles only

--- neovim/.config/nvim/after/plugin/telescope.lua
+++ neovim/.config/nvim/after/plugin/telescope.lua
@@ -1,6 +1,21 @@
+require("telescope").setup({
+    defaults = {
+        preview = false,
+    },
+})
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>ft', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+local search_dotfiles = function()
+	builtin.find_files({
+		prompt_title = "< dotfiles >",
+		cwd = "~/.dotfiles/",
+		hidden = true,
+	})
+end
+vim.keymap.set('n', '<leader>fc', search_dotfiles, {})
