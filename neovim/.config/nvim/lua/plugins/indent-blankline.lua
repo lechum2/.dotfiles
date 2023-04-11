ae365659c203831699560e0b7eb04b4495bddd84
Sebastian Lech <y2dx@nykredit.dk>
2023-04-11 10:01:08 +0200

    Set icons for Consolas font used on my windows setup

--- neovim/.config/nvim/lua/plugins/indent-blankline.lua
+++ neovim/.config/nvim/lua/plugins/indent-blankline.lua
@@ -7,8 +7,8 @@ return {
     },
     init = function()
         vim.opt.list = true
-        vim.opt.listchars:append("space:⋅")
-        vim.opt.listchars:append("eol:↴")
-        vim.opt.listchars:append("tab:  ⇥")
+        vim.opt.listchars:append("space:·")
+        vim.opt.listchars:append("eol:╮")
+        vim.opt.listchars:append("tab:  ¬")
     end
 }
