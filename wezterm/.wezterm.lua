e78f3d08193c4acd424efe89b5e7ba056aa64ce8
Sebastian Lech <y2dx@nykredit.dk>
2023-05-10 12:46:22 +0200

    Add keybindings I use

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -7,7 +7,7 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+    config = wezterm.config_builder()
 end
 
 -- This is where you actually apply your config choices
@@ -23,6 +23,36 @@ config.font_size = 11
 
 config.default_prog = { 'C:\\Program Files\\PowerShell\\7\\pwsh.exe' }
 
+local act = wezterm.action
+config.keys = {
+    {
+        key = 'h',
+        mods = 'CTRL|SHIFT',
+        action = act.MoveTabRelative(-1),
+    },
+    {
+        key = 'l',
+        mods = 'CTRL|SHIFT',
+        action = act.MoveTabRelative(1),
+    },
+    {
+        key = 'n',
+        mods = 'CTRL|SHIFT',
+        action = act.SpawnTab('DefaultDomain'),
+    },
+    {
+        key = '<',
+        mods = 'CTRL|SHIFT',
+        action = act.SpawnTab('CurrentPaneDomain'),
+    },
+    {
+        key = 'm',
+        mods = 'CTRL|SHIFT',
+        action = act.SpawnCommandInNewTab {
+            args = { 'nvim' },
+            domain = 'CurrentPaneDomain',
+        },
+    },
+}
 -- and finally, return the configuration to wezterm
 return config
-
