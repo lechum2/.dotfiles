d1562dab35b5219db999c12ff381feeb1c491b07
Sebastian Lech <y2dx@nykredit.dk>
2023-08-11 15:32:05 +0200

    Add shortcuts for history search that I am used to

--- powershell/.config/powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/.config/powershell/Microsoft.PowerShell_profile.ps1
@@ -2,4 +2,7 @@ Invoke-Expression (&starship init powershell)
 
 Import-Module posh-git
 
+Set-PSReadLineKeyHandler -Chord Ctrl+n HistorySearchForward
+Set-PSReadLineKeyHandler -Chord Ctrl+p HistorySearchBackward
+
 New-Alias l ls
