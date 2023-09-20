47eb9c09bbed4cfdc16fb1b43c65beb2cc800e60
Sebastian Lech <y2dx@nykredit.dk>
2023-09-20 14:26:49 +0200

    allow wezterm to get current operating path from powershell with starship

--- powershell/.config/powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/.config/powershell/Microsoft.PowerShell_profile.ps1
@@ -6,3 +6,14 @@ Set-PSReadLineKeyHandler -Chord Ctrl+n HistorySearchForward
 Set-PSReadLineKeyHandler -Chord Ctrl+p HistorySearchBackward
 
 New-Alias l ls
+
+$prompt = ""
+function Invoke-Starship-PreCommand {
+    $current_location = $executionContext.SessionState.Path.CurrentLocation
+    if ($current_location.Provider.Name -eq "FileSystem") {
+        $ansi_escape = [char]27
+        $provider_path = $current_location.ProviderPath -replace "\\", "/"
+        $prompt = "$ansi_escape]7;file://${env:COMPUTERNAME}/${provider_path}$ansi_escape\"
+    }
+    $host.ui.Write($prompt)
+}
