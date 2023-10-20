2887fadd38a7a7cecc9925abc4166200b55426cb
Sebastian Lech <lechum2@gmail.com>
2023-10-20 12:19:32 +0200

    Remove not needed sections

--- neovim/.config/nvim/lua/plugins/startup.lua
+++ neovim/.config/nvim/lua/plugins/startup.lua
@@ -25,7 +25,7 @@ return {
             default_color = "",
             oldfiles_amount = 0,
         },
-        header_2 = {
+        body = {
             type = "text",
             oldfiles_directory = false,
             align = "center",
@@ -44,40 +44,8 @@ return {
             default_color = "",
             oldfiles_amount = 0,
         },
-        body = {
-            type = "mapping",
-            oldfiles_directory = false,
-            align = "center",
-            fold_section = false,
-            title = "Basic Commands",
-            margin = 5,
-            content = {
-                { " Find File", "Telescope find_files", "<leader>ff" },
-                { " Find Word", "Telescope live_grep", "<leader>lg" },
-                { " Recent Files", "Telescope oldfiles", "<leader>of" },
-                { " File Browser", "Telescope file_browser", "<leader>fb" },
-                { " Colorschemes", "Telescope colorscheme", "<leader>cs" },
-                { " New File", "lua require'startup'.new_file()", "<leader>nf" },
-            },
-            highlight = "String",
-            default_color = "",
-            oldfiles_amount = 0,
-        },
-        footer = {
-            type = "text",
-            oldfiles_directory = false,
-            align = "center",
-            fold_section = false,
-            title = "Footer",
-            margin = 5,
-            content = { "startup.nvim" },
-            highlight = "Number",
-            default_color = "",
-            oldfiles_amount = 0,
-        },
-
         options = {
-            mapping_keys = true,
+            mapping_keys = false,
             cursor_column = 0.5,
             empty_lines_between_mappings = true,
             disable_statuslines = true,
@@ -91,9 +59,7 @@ return {
             open_help = "?",
         },
         colors = {
-            -- background = "#1f2227",
-            -- folded_section = "#56b6c2",
         },
-        parts = { "header", "header_2", "body", "footer" },
+        parts = { "header", "body", },
     },
 }
