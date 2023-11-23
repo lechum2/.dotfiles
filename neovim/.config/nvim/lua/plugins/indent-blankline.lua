80cb3d3519e9aa0123348be671602bfdcefbd44f
Sebastian Lech <y2dx@nykredit.dk>
2023-11-23 12:17:44 +0100

    Use middle dot for now; need to find more subtle char

--- neovim/.config/nvim/lua/plugins/indent-blankline.lua
+++ neovim/.config/nvim/lua/plugins/indent-blankline.lua
@@ -6,7 +6,7 @@ return {
     },
     init = function()
         vim.opt.list = true
-        vim.opt.listchars:append("space:⋅")
+        vim.opt.listchars:append("space:·")
         vim.opt.listchars:append("eol:↴")
         vim.opt.listchars:append("tab:  ⇥")
     end
