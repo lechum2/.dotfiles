a24da4ff36ec0dfe2dca40961f09606a992c2761
Sebastian Lech <lechum2@gmail.com>
2023-12-20 21:47:29 +0100

    Add keymaps for copping file, file path and relative file path

--- neovim/.config/nvim/lua/custom/remap.lua
+++ neovim/.config/nvim/lua/custom/remap.lua
@@ -11,3 +11,6 @@ vim.keymap.set("n", "N", "Nzzzv")
 
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set("n", "<leader>yp", function() vim.fn.setreg('+', vim.fn.expand('%:p')) end)
+vim.keymap.set("n", "<leader>yf", function() vim.fn.setreg('+', vim.fn.expand('%:t')) end)
+vim.keymap.set("n", "<leader>yr", function() vim.fn.setreg('+', vim.fn.expand('%')) end)
