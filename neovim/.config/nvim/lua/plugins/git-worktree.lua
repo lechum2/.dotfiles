c2d66ee57b75c9be809063827890a92a9cc8807a
Sebastian Lech <lechum2@gmail.com>
2023-12-21 20:27:35 +0100

    Lazy load git worktree plugin

--- neovim/.config/nvim/lua/plugins/git-worktree.lua
+++ neovim/.config/nvim/lua/plugins/git-worktree.lua
@@ -1,6 +1,5 @@
 return {
     "ThePrimeagen/git-worktree.nvim",
-    lazy = true,
     opts = {
         change_directory_command = "cd",
         update_on_change = true,
@@ -8,11 +7,11 @@ return {
         clearjumps_on_change = true,
         autopush = false,
     },
-    init = function()
+    config = function()
         require("telescope").load_extension("git_worktree")
     end,
     keys = {
         { "<leader>fw", function () require('telescope').extensions.git_worktree.git_worktrees() end },
-        { "<leader>fc", function () require('telescope').extensions.git_worktree.create_git_worktree() end  },
+        { "<leader>fb", function () require('telescope').extensions.git_worktree.create_git_worktree() end  },
     },
 }
