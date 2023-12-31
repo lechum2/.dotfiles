11053d5296c7951429b3c46a95c7c01d41cee962
Sebastian Lech <lechum2@gmail.com>
2023-12-31 10:30:08 +0100

    Add commenting settings for apex

--- neovim/.config/nvim/lua/plugins/sfdx.lua
+++ neovim/.config/nvim/lua/plugins/sfdx.lua
@@ -5,5 +5,8 @@ return {
         local sfdx = require("sfdx")
         vim.keymap.set("n", "<leader>sd", sfdx.deployCurrentFile, {})
         vim.keymap.set("n", "<leader>stf", sfdx.runTestsFromCurrentFile, {})
+
+        local ft = require("Comment.ft")
+        ft.set("apex", { "//%s", "/*%s*/" })
     end,
 }
