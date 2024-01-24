9839690d8b3febbe6bc0a51bd0f9ad5bac15de3e
Sebastian Lech <lechum2@gmail.com>
2024-01-24 20:54:56 +0100

    Show alias in lualine(load salesforce extension) only for apex or xml files

--- neovim/.config/nvim/lua/plugins/lualine.lua
+++ neovim/.config/nvim/lua/plugins/lualine.lua
@@ -1,20 +1,33 @@
 local function salesforce()
-    local org_manager = require('salesforce.org_manager');
+    local org_manager = require("salesforce.org_manager")
     local alias = org_manager:get_default_alias()
     if alias == nil then
         return ""
     end
     return alias
 end
+
+local salesforce_extension = {
+    sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = { "filename", { salesforce, icon = "󰢎" } },
+        lualine_x = { "encoding", "fileformat", "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
+    },
+    filetypes = { "apex", "xml" },
+}
+
 return {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons', opt = true },
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons", opt = true },
     opts = {
         options = {
             icons_enabled = true,
-            theme = 'auto',
-            component_separators = { left = '', right = '' },
-            section_separators = { left = '', right = '' },
+            theme = "auto",
+            component_separators = { left = "", right = "" },
+            section_separators = { left = "", right = "" },
             disabled_filetypes = {
                 statusline = {},
                 winbar = {},
@@ -26,27 +39,27 @@ return {
                 statusline = 1000,
                 tabline = 1000,
                 winbar = 1000,
-            }
+            },
         },
         sections = {
-            lualine_a = { 'mode' },
-            lualine_b = { 'branch', 'diff', 'diagnostics' },
-            lualine_c = { 'filename', { salesforce, icon = "󰢎" } },
-            lualine_x = { 'encoding', 'fileformat', 'filetype' },
-            lualine_y = { 'progress' },
-            lualine_z = { 'location' }
+            lualine_a = { "mode" },
+            lualine_b = { "branch", "diff", "diagnostics" },
+            lualine_c = { "filename" },
+            lualine_x = { "encoding", "fileformat", "filetype" },
+            lualine_y = { "progress" },
+            lualine_z = { "location" },
         },
         inactive_sections = {
             lualine_a = {},
             lualine_b = {},
-            lualine_c = { 'filename' },
-            lualine_x = { 'location' },
+            lualine_c = { "filename" },
+            lualine_x = { "location" },
             lualine_y = {},
-            lualine_z = {}
+            lualine_z = {},
         },
         tabline = {},
         winbar = {},
         inactive_winbar = {},
-        extensions = {}
-    }
+        extensions = { "oil", salesforce_extension },
+    },
 }
