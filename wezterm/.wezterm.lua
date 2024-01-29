c42eaaf4764fb25779264e7fcce010cafd5aaf4e
Sebastian Lech <y2dx@nykredit.dk>
2024-01-29 10:52:42 +0100

    Start wezterm window maximized

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -97,5 +97,13 @@ config.keys = {
         },
     },
 }
+
+-- start maximized
+local mux = wezterm.mux
+wezterm.on('gui-startup', function(cmd)
+  local tab, pane, window = mux.spawn_window(cmd or {})
+  window:gui_window():maximize()
+end)
+
 -- and finally, return the configuration to wezterm
 return config
