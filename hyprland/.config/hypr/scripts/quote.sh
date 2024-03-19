124dcfefc7a83872e040b7a6cc1be08f6826237d
Sebastian Lech <lechum2@gmail.com>
2024-03-19 16:00:47 +0100

    Delay to make sure waybar shows before

--- hyprland/.config/hypr/scripts/quote.sh
+++ hyprland/.config/hypr/scripts/quote.sh
@@ -2,4 +2,5 @@
 quoteJson=$(curl https://api.quotable.io/random -s)
 quote=$(echo "$quoteJson" | jq --raw-output ".content")
 author=$(echo "$quoteJson" | jq --raw-output ".author")
+sleep 2
 notify-send "$author" "$quote"
