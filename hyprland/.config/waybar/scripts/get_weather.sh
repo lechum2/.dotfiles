dcb296a885bbe2c5439142dedcf63b4c3845e899
Sebastian Lech <lechum2@gmail.com>
2024-03-29 07:46:22 +0100

    Set default location to warsaw

--- hyprland/.config/waybar/scripts/get_weather.sh
+++ hyprland/.config/waybar/scripts/get_weather.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 for i in {1..5}
 do
-    text=$(curl -s "https://wttr.in/$1?format=1")
+    text=$(curl -s "https://wttr.in/warsaw?format=1")
     if [[ $? == 0 ]]
     then
         text=$(echo "$text" | sed -E "s/\s+/ /g")
-        tooltip=$(curl -s "https://wttr.in/$1?format=4")
+        tooltip=$(curl -s "https://wttr.in/warsaw?format=4")
         if [[ $? == 0 ]]
         then
             tooltip=$(echo "$tooltip" | sed -E "s/\s+/ /g")
