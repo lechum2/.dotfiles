4a828d125047f39fc917d89888b95f307b6e88e8
Sebastian Lech <lechum2@gmail.com>
2024-05-17 14:19:45 +0200

    Set proper aliases on org entries

--- docker/sfRestoreOrgs.sh
+++ docker/sfRestoreOrgs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for file in /home/lechu/sfAuthFiles/*; do
-    sf org login sfdx-url --sfdx-url-file "$file"
+    sf org login sfdx-url --sfdx-url-file "$file" --alias $(basename $file .json)
 done
 
 rm -rf /home/lechu/sfAuthFiles
