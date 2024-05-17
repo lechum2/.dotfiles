90fc6d67a55209e5335b8acca13e162872b16cc7
Sebastian Lech <lechum2@gmail.com>
2024-05-17 14:14:20 +0200

    Get all entries from sf but filter by aliases

--- docker/sfSaveOrgs.js
+++ docker/sfSaveOrgs.js
@@ -6,10 +6,19 @@ const fs = require('fs');
 function getAllLocalOrgs() {
     const orgs = execSync(`sf org list --json`);
     const result = JSON.parse(orgs.toString());
+    const unique = {};
     return result.result.other
         .concat(result.result.sandboxes)
         .concat(result.result.devHubs)
-        .concat(result.result.scratchOrgs);
+        .concat(result.result.scratchOrgs)
+        .concat(result.result.nonScratchOrgs)
+        .filter(function(org) {
+            if(unique[org.alias]) {
+                return false;
+            }
+            unique[org.alias] = true;
+            return true;
+        });
 }
 
 const allOrgs = getAllLocalOrgs();
