c9a64b51ca05cdc91ee1cda86ce05195d744f051
Sebastian Lech <lechum2@gmail.com>
2024-06-21 14:14:19 +0200

    Add detection that nvim is run on android

--- neovim/.config/nvim/lua/plugins/codeium.lua
+++ neovim/.config/nvim/lua/plugins/codeium.lua
@@ -4,9 +4,13 @@ return {
         "nvim-lua/plenary.nvim",
         "hrsh7th/nvim-cmp",
     },
-    opts = {
-        tools = {
-            language_server = "/data/data/com.termux/files/home/codeium_lsp_proot.sh"
-        }
-    }
+    config = function()
+        if vim.uv.os_uname().release:match("android") then
+            require("codeium").setup({
+                tools = {
+                    language_server = "/data/data/com.termux/files/home/codeium_lsp_proot.sh"
+                }
+            })
+        end
+    end
 }
