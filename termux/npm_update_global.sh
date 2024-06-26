184ac4ead865eafa05a6d6c0c64ac645f9f1a339
Sebastian Lech <lechum2@gmail.com>
2024-06-26 11:02:22 +0200

    fix replace string

--- termux/npm_update_global.sh
+++ termux/npm_update_global.sh
@@ -2,4 +2,4 @@
 
 npm update -g
 
-sed -i -- 's/\'linux\'/\'android\'/g' /data/data/com.termux/files/home/.local/lib/node_modules/@salesforce/cli/node_modules/@salesforce/core/lib/crypto/keyChain.js
+sed -i -- "s/'linux'/'android'/g" /data/data/com.termux/files/home/.local/lib/node_modules/@salesforce/cli/node_modules/@salesforce/core/lib/crypto/keyChain.js
