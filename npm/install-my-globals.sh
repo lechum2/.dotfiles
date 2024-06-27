d93bd7e814ea111a7ec856ca769068f998425768
Sebastian Lech <lechum2@gmail.com>
2024-06-27 12:16:53 +0200

    Save global npm packages I use

---
+++ npm/install-my-globals.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+npm install --global \
+    @prettier/plugin-xml \
+    bash-language-server \
+    eslint \
+    neovim \
+    npm-groovy-lint \
+    prettier \
+    prettier-plugin-apex \
+    typescript \
+    typescript-language-server \
+    vscode-langservers-extracted \
+    yaml-language-server \
+    yarn
