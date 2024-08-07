4388c1429e015bede13f39407dbfb1ec1e76aab0
Sebastian Lech <lechum2@gmail.com>
2024-08-07 14:36:15 +0200

    Fix execution of bash-language-server from within neovim

--- termux/npm_update_global.sh
+++ termux/npm_update_global.sh
@@ -5,3 +5,4 @@ npm update -g
 sed -i -- "s/'linux'/'android'/g" /data/data/com.termux/files/home/.local/lib/node_modules/@salesforce/cli/node_modules/@salesforce/core/lib/crypto/keyChain.js
 
 termux-fix-shebang /data/data/com.termux/files/home/.local/bin/typescript-language-server
+termux-fix-shebang /data/data/com.termux/files/home/.local/bin/bash-language-server
