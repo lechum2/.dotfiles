8e144f1ff97712bbe421fab5aa92cc270647eda4
Sebastian Lech <lechum2@gmail.com>
2024-08-08 09:14:54 +0200

    Make all the scripts in node installation folder executable by neovim processes

--- termux/npm_update_global.sh
+++ termux/npm_update_global.sh
@@ -4,5 +4,6 @@ npm update -g
 
 sed -i -- "s/'linux'/'android'/g" /data/data/com.termux/files/home/.local/lib/node_modules/@salesforce/cli/node_modules/@salesforce/core/lib/crypto/keyChain.js
 
-termux-fix-shebang /data/data/com.termux/files/home/.local/bin/typescript-language-server
-termux-fix-shebang /data/data/com.termux/files/home/.local/bin/bash-language-server
+for file in /data/data/com.termux/files/home/.local/bin; do
+    termux-fix-shebang "$file"
+done
