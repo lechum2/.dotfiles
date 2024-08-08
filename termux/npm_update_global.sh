feab878dd494e462d2530f1b3614ec7fc7a986e1
Sebastian Lech <lechum2@gmail.com>
2024-08-08 10:07:31 +0200

    Check if it is a file before change

--- termux/npm_update_global.sh
+++ termux/npm_update_global.sh
@@ -4,6 +4,10 @@ npm update -g
 
 sed -i -- "s/'linux'/'android'/g" /data/data/com.termux/files/home/.local/lib/node_modules/@salesforce/cli/node_modules/@salesforce/core/lib/crypto/keyChain.js
 
-for file in /data/data/com.termux/files/home/.local/bin; do
-    termux-fix-shebang "$file"
+for file in /data/data/com.termux/files/home/.local/bin/*; do
+    if [ -f "$file" ];
+    then
+        echo "Changing shebang in $file"
+        termux-fix-shebang "$file"
+    fi
 done
