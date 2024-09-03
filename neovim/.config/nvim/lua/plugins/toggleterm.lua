c55849cfe2458df842cd4a9fbe1977246fd365a8
Sebastian Lech <x9t@bec.dk>
2024-09-03 12:02:29 +0000

    map esc to work in terminal mode

--- neovim/.config/nvim/lua/plugins/toggleterm.lua
+++ neovim/.config/nvim/lua/plugins/toggleterm.lua
@@ -6,6 +6,7 @@ return {
         { "<leader>tl", "<cmd>ToggleTermSendCurrentLine<cr>" },
         { "<leader>ts", "<cmd>ToggleTermSendVisualSelection<cr>", mode = "v" },
         { "<C-k>", "<cmd>wincmd k<cr>", mode = "t" },
+        { "<esc>", "<C-\\><C-n>", mode = "t" },
     },
     opts = {
         size = 8
