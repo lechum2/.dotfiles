5ae7042afd6ff15388955f8cd5226e1e0b3ef4ae
Sebastian Lech <x9t@bec.dk>
2024-10-24 12:44:13 +0000

    format

--- neovim/.config/nvim/lua/plugins/which-key.lua
+++ neovim/.config/nvim/lua/plugins/which-key.lua
@@ -1,14 +1,14 @@
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  config = true,
-  keys = {
-    {
-      "<leader>?",
-      function()
-        require("which-key").show({ global = false })
-      end,
-      desc = "Buffer Local Keymaps (which-key)",
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    config = true,
+    keys = {
+        {
+            "<leader>?",
+            function()
+                require("which-key").show({ global = false })
+            end,
+            desc = "Buffer Local Keymaps (which-key)",
+        },
     },
-  },
 }
