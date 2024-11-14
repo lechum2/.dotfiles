316351d4cc954c3d3f710f02cacbf6695fd60d7d
Sebastian Lech <lechum2@gmail.com>
2024-11-14 14:58:02 +0000

    Add gruvbox

--- neovim/.config/nvim/lua/plugins/themery.lua
+++ neovim/.config/nvim/lua/plugins/themery.lua
@@ -6,6 +6,21 @@ return {
             themes = {
                 "ayu-dark",
                 "ayu-light",
+                {
+                    name = "gruvbox-dark",
+                    colorscheme = "gruvbox",
+                    before = [[
+                    -- All this block will be executed before apply "set colorscheme"
+                    vim.opt.background = "dark"
+                    ]],
+                },
+                {
+                    name = "gruvbox-light",
+                    colorscheme = "gruvbox",
+                    before = [[
+                    vim.opt.background = "light"
+                    ]],
+                },
                 "midnight",
                 "monoglow",
                 "moonbow",
@@ -16,6 +31,12 @@ return {
         "Shatur/neovim-ayu",
         lazy = false,
         priority = 1000,
+    },
+    {
+        "ellisonleao/gruvbox.nvim",
+        priority = 1000,
+        config = true,
+        lazy = false,
     },
     {
         "dasupradyumna/midnight.nvim",
