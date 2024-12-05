33a13176353c1a574a80cdcdd41ec047b88476d3
Sebastian Lech <lechum2@gmail.com>
2024-12-05 13:45:32 +0100

    Add fun functions to code

---
+++ neovim/.config/nvim/lua/plugins/fun.lua
@@ -0,0 +1,10 @@
+return {{
+    "marcussimonsen/let-it-snow.nvim",
+    cmd = "LetItSnow", -- Wait with loading until command is run
+    opts = {},
+},
+{
+    "eandrju/cellular-automaton.nvim",
+    cmd = "CellularAutomaton"
+},
+}
