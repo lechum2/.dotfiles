fa35fe7dd2d69bb8023a043b09680adc5164cfa2
Sebastian Lech <x9t@bec.dk>
2024-12-05 12:48:24 +0000

    format

--- neovim/.config/nvim/lua/plugins/fun.lua
+++ neovim/.config/nvim/lua/plugins/fun.lua
@@ -1,10 +1,11 @@
-return {{
-    "marcussimonsen/let-it-snow.nvim",
-    cmd = "LetItSnow", -- Wait with loading until command is run
-    opts = {},
-},
-{
-    "eandrju/cellular-automaton.nvim",
-    cmd = "CellularAutomaton"
-},
+return {
+    {
+        "marcussimonsen/let-it-snow.nvim",
+        cmd = "LetItSnow", -- Wait with loading until command is run
+        opts = {},
+    },
+    {
+        "eandrju/cellular-automaton.nvim",
+        cmd = "CellularAutomaton",
+    },
 }
