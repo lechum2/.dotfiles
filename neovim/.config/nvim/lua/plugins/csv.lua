ea2ba91810698317db9747e43c2db33f6c98281b
Sebastian Lech <x9t@bec.dk>
2025-02-17 09:21:24 +0000

    Switch to lua version

--- neovim/.config/nvim/lua/plugins/csv.lua
+++ neovim/.config/nvim/lua/plugins/csv.lua
@@ -1,7 +1,22 @@
 return {
     {
-        "mechatroner/rainbow_csv",
-        ft = { "csv" },
+        "cameron-wags/rainbow_csv.nvim",
+        config = true,
+        ft = {
+            "csv",
+            "tsv",
+            "csv_semicolon",
+            "csv_whitespace",
+            "csv_pipe",
+            "rfc_csv",
+            "rfc_semicolon",
+        },
+        cmd = {
+            "RainbowDelim",
+            "RainbowDelimSimple",
+            "RainbowDelimQuoted",
+            "RainbowMultiDelim",
+        },
     },
     {
         "hat0uma/csvview.nvim",
