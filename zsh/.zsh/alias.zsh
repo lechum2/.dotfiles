a76ba8a23d9021156876892fd0e8bf6b51b7ffc3
Sebastian Lech <x9t@bec.dk>
2025-02-20 09:26:34 +0000

    Create a sperate alias

--- zsh/.zsh/alias.zsh
+++ zsh/.zsh/alias.zsh
@@ -1,5 +1,6 @@
 alias pip=pip3
 alias vsc='code --enable-features=UseOzonePlatform --ozone-platform=wayland .'
 alias H=Hyprland
-alias up='yay --noconfirm && flatpak update --noninteractive && npm upgrade -g && rustup update && sf update && wget -O ~/.local/share/nvim/apex-jorje-lsp.jar "https://github.com/forcedotcom/salesforcedx-vscode/blob/develop/packages/salesforcedx-vscode-apex/out/apex-jorje-lsp.jar?raw=true"'
+alias up='yay --noconfirm && flatpak update --noninteractive && npm upgrade -g && rustup update && sf update'
 alias C=start-cosmic
+alias update_apex_ls='wget -O ~/.local/share/nvim/apex-jorje-lsp.jar "https://github.com/forcedotcom/salesforcedx-vscode/blob/develop/packages/salesforcedx-vscode-apex/out/apex-jorje-lsp.jar?raw=true"'
