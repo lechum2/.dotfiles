d7af567d4a1787e4698dd1b2febbed4e7b7d4a70
Sebastian Lech <x9t@bec.dk>
2025-02-21 13:15:37 +0000

    Add lua_ls configuration for neovim functions

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -90,7 +90,51 @@ return {
             })
 
             local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({})
+            lspconfig.lua_ls.setup({
+                settings = {
+                    Lua = {
+                        telemetry = {
+                            enable = false,
+                        },
+                    },
+                },
+                on_init = function(client)
+                    local join = vim.fs.joinpath
+                    local path = client.workspace_folders[1].name
+
+                    -- Don't do anything if there is project local config
+                    if vim.uv.fs_stat(join(path, ".luarc.json")) or vim.uv.fs_stat(join(path, ".luarc.jsonc")) then
+                        return
+                    end
+
+                    -- Apply neovim specific settings
+                    local runtime_path = vim.split(package.path, ";")
+                    table.insert(runtime_path, join("lua", "?.lua"))
+                    table.insert(runtime_path, join("lua", "?", "init.lua"))
+
+                    local nvim_settings = {
+                        runtime = {
+                            -- Tell the language server which version of Lua you're using
+                            version = "LuaJIT",
+                            path = runtime_path,
+                        },
+                        diagnostics = {
+                            -- Get the language server to recognize the `vim` global
+                            globals = { "vim" },
+                        },
+                        workspace = {
+                            checkThirdParty = false,
+                            library = {
+                                -- Make the server aware of Neovim runtime files
+                                vim.env.VIMRUNTIME,
+                                vim.fn.stdpath("config"),
+                            },
+                        },
+                    }
+
+                    client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, nvim_settings)
+                end,
+            })
             lspconfig.rust_analyzer.setup({})
             lspconfig.bashls.setup({})
             lspconfig.eslint.setup({})
