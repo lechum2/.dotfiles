62596d5d61502874c550a5a2980e9f259b214560
Sebastian Lech <lechum2@gmail.com>
2025-04-09 15:10:41 +0200

    Add simple one-liner for apex lsp installation

---
+++ scripts/update_apex_lsp.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+wget -O ~/.local/share/nvim/apex-jorje-lsp.jar 'https://github.com/forcedotcom/salesforcedx-vscode/blob/develop/packages/salesforcedx-vscode-apex/out/apex-jorje-lsp.jar?raw=true'
