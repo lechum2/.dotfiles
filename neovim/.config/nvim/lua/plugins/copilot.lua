ca3295216a00e9fb3353f6b529b1cb878ee0f466
Sebastian Lech <lechum2@gmail.com>
2025-06-30 16:01:41 +0200

    use default branch

--- neovim/.config/nvim/lua/plugins/copilot.lua
+++ neovim/.config/nvim/lua/plugins/copilot.lua
@@ -9,7 +9,6 @@ return {
     },
     {
         "CopilotC-Nvim/CopilotChat.nvim",
-        branch = "canary",
         dependencies = {
             { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
             { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
