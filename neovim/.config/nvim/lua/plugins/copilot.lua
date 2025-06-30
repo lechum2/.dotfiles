f18be0cb855578e4ceefb468ee642b1d4a941a95
Sebastian Lech <lechum2@gmail.com>
2025-06-30 16:17:58 +0200

    Remove not needed option

--- neovim/.config/nvim/lua/plugins/copilot.lua
+++ neovim/.config/nvim/lua/plugins/copilot.lua
@@ -13,7 +13,7 @@ return {
             { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
             { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
         },
-        build = "make tiktoken", -- Only on MacOS or Linux
+        -- build = "make tiktoken", -- Only on MacOS or Linux
         opts = {
             debug = false, -- Enable debugging
             -- See Configuration section for rest
