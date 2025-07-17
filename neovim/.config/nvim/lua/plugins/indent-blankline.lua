ffb9e787c84dd92bd06566850b141cc5850f4acc
Sebastian Lech <lechum2@gmail.com>
2025-07-17 10:27:33 +0200

    Remove lines on dashboard

--- neovim/.config/nvim/lua/plugins/indent-blankline.lua
+++ neovim/.config/nvim/lua/plugins/indent-blankline.lua
@@ -3,11 +3,32 @@ return {
     lazy = false,
     main = "ibl",
     opts = {
+        exclude = {
+            filetypes = {
+                "",
+                "TelescopePrompt",
+                "TelescopeResults",
+                "checkhealth",
+                "dashboard",
+                "gitcommit",
+                "help",
+                "lazy",
+                "lspinfo",
+                "man",
+                "packer",
+            },
+            buftypes = {
+                "nofile",
+                "prompt",
+                "quickfix",
+                "terminal",
+            },
+        },
     },
     init = function()
         vim.opt.list = true
         vim.opt.listchars:append("space:⋅")
         vim.opt.listchars:append("eol:↴")
         vim.opt.listchars:append("tab:  ⇥")
-    end
+    end,
 }
