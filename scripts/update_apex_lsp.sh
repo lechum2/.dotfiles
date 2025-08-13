2255cedf12a7f04cad8fbc3b3eb280f285592b9e
Sebastian Lech <lechum2@gmail.com>
2025-08-13 16:25:21 +0200

    Update update_apex_lsp.sh

--- scripts/update_apex_lsp.sh
+++ scripts/update_apex_lsp.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 path=$(nvim --headless +'lua print(vim.fn.stdpath("data"))' +qa)
-echo `$path/apex-jorje-lsp.jar`
-wget -O `$path/apex-jorje-lsp.jar` https://raw.githubusercontent.com/forcedotcom/salesforcedx-vscode/develop/packages/salesforcedx-vscode-apex/jars/apex-jorje-lsp.jar
+echo "$path/apex-jorje-lsp.jar"
+wget -O "$path/apex-jorje-lsp.jar" https://raw.githubusercontent.com/forcedotcom/salesforcedx-vscode/develop/packages/salesforcedx-vscode-apex/jars/apex-jorje-lsp.jar
