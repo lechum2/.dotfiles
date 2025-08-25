3dbaf419905a41b11062c16b6eb8223a3da8c281
Sebastian Lech <lechum2@gmail.com>
2025-08-25 13:12:39 +0200

    mv needs directory

--- scripts/update_apex_lsp.sh
+++ scripts/update_apex_lsp.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget https://raw.githubusercontent.com/forcedotcom/salesforcedx-vscode/develop/packages/salesforcedx-vscode-apex/jars/apex-jorje-lsp.jar
 neovimDataFolder=$(nvim --headless +'lua print(vim.fn.stdpath("data"))' +qa 2>&1)
-mv -f ./apex-jorje-lsp.jar $neovimDataFolder/apex-jorje-lsp.jar
+mv --force apex-jorje-lsp.jar $neovimDataFolder
