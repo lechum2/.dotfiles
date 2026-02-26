80b566975665541d5d819c3c62dc4cf8173ffeed
Sebastian Lech <lechum2@gmail.com>
2026-02-26 16:06:07 +0100

    Add mcp

--- neovim/.config/nvim/lua/plugins/copilot.lua
+++ neovim/.config/nvim/lua/plugins/copilot.lua
@@ -5,7 +5,7 @@ return {
         opts = {
             suggestion = { enabled = false },
             panel = { enabled = false },
-            copilot_model = "gpt-5.2-mini",
+            copilot_model = "gpt-4o",
         },
     },
     {
@@ -29,4 +29,21 @@ return {
         -- See Commands section for default commands if you want to lazy load on them
         cmd = { "CopilotChatOpen" },
     },
+    {
+        "ravitemer/mcphub.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        build = "npm install -g mcp-hub@latest", -- Installs `mcp-hub` node binary globally
+        opts = {
+            extenstions = {
+                copilotchat = {
+                    enabled = true,
+                    convert_tools_to_functions = true, -- Convert MCP tools to CopilotChat functions
+                    convert_resources_to_functions = true, -- Convert MCP resources to CopilotChat functions
+                    add_mcp_prefix = false,            -- Add "mcp_" prefix to function names
+                }
+            }
+        }
+    }
 }
