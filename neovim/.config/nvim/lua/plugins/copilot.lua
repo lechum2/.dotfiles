0d82b5ce2723735f8dd12ce90fba8fca0606f92f
Sebastian Lech <lechum2@gmail.com>
2026-02-26 16:26:10 +0100

    Change models

--- neovim/.config/nvim/lua/plugins/copilot.lua
+++ neovim/.config/nvim/lua/plugins/copilot.lua
@@ -5,7 +5,7 @@ return {
         opts = {
             suggestion = { enabled = false },
             panel = { enabled = false },
-            copilot_model = "gpt-4o",
+            copilot_model = "gpt-41-copilot",
         },
     },
     {
