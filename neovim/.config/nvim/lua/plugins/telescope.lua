f763b1adad229958d7c3fc32bedbc38e3a6f2857
Sebastian Lech <lechum2@gmail.com>
2026-03-11 09:50:15 +0100

    Update telescope

--- neovim/.config/nvim/lua/plugins/telescope.lua
+++ neovim/.config/nvim/lua/plugins/telescope.lua
@@ -1,9 +1,12 @@
 return {
     "nvim-telescope/telescope.nvim",
-    version = "0.1.x",
+    version = "*",
     dependencies = {
         { "nvim-lua/plenary.nvim" },
-        { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+        {
+            "nvim-telescope/telescope-fzf-native.nvim",
+            build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release --target install",
+        },
     },
     opts = {
         defaults = {
