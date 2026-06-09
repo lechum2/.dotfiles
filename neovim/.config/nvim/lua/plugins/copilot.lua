e4d702f10bd05e9324a688d098eee97991e71674
Sebastian Lech <lechum2@gmail.com>
2026-06-09 11:23:43 +0200

    Remove mcphub; was not using it

--- neovim/.config/nvim/lua/plugins/copilot.lua
+++ neovim/.config/nvim/lua/plugins/copilot.lua
@@ -22,55 +22,19 @@ return {
             },
             auto_insert_mode = true, -- Enter insert mode when opening
             debug = false, -- Enable debugging
-            -- See Configuration section for rest
+            trusted_tools = { "copilot", "curl", "pwsh", "python", "node", "rg", "jq" },
         },
         -- See Commands section for default commands if you want to lazy load on them
         cmd = { "CopilotChatOpen" },
-    },
-    {
-        "ravitemer/mcphub.nvim",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-        },
-        build = "npm install -g mcp-hub@latest", -- Installs `mcp-hub` node binary globally
-        opts = {
-            extensions = {
-                copilotchat = {
-                    enabled = true,
-                    convert_tools_to_functions = true, -- Convert MCP tools to CopilotChat functions
-                    convert_resources_to_functions = true, -- Convert MCP resources to CopilotChat functions
-                    add_mcp_prefix = false, -- Add "mcp_" prefix to function names
-                },
-            },
-        },
-        cmd = { "MCPHub" },
     },
     {
         "olimorris/codecompanion.nvim",
         dependencies = {
             "nvim-lua/plenary.nvim",
             "nvim-treesitter/nvim-treesitter",
-            "ravitemer/mcphub.nvim",
             "MeanderingProgrammer/render-markdown.nvim",
         },
         opts = {
-            extensions = {
-                mcphub = {
-                    callback = "mcphub.extensions.codecompanion",
-                    opts = {
-                        -- MCP Tools
-                        make_tools = true, -- Make individual tools (@server__tool) and server groups (@server) from MCP servers
-                        show_server_tools_in_chat = true, -- Show individual tools in chat completion (when make_tools=true)
-                        add_mcp_prefix_to_tool_names = false, -- Add mcp__ prefix (e.g `@mcp__github`, `@mcp__neovim__list_issues`)
-                        show_result_in_chat = true, -- Show tool results directly in chat buffer
-                        format_tool = nil, -- function(tool_name:string, tool: CodeCompanion.Agent.Tool) : string Function to format tool names to show in the chat buffer
-                        -- MCP Resources
-                        make_vars = false, -- Convert MCP resources to #variables for prompts
-                        -- MCP Prompts
-                        make_slash_commands = true, -- Add MCP prompts as /slash commands
-                    },
-                },
-            },
             -- interactions = {
             --     chat = {
             --         adapter = "copilot",
