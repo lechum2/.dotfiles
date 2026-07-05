738d749fde54e1370b8d3bc5f8f03136f45c6678
Sebastian Lech <lechum2@gmail.com>
2026-07-05 15:02:50 +0200

    Add lsp for c

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -144,6 +144,7 @@ return {
             vim.lsp.enable("lua_ls")
             vim.lsp.enable("rust_analyzer")
             vim.lsp.enable("bashls")
+            vim.lsp.enable('clangd')
             vim.lsp.config("jsonls", {
                 settings = {
                     json = {
